0e0069b2b6d43ad70285b42a89dd6a4f0b5ae60e
scossar <scossar3@gmail.com>
2016-07-15 11:45:21 -0700

    Initial commit

---
+++ email-address-verification.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Email Address Verification
+ * Description: Validates WordPress user's email addresses
+ * Version: 0.1.0
+ * Text-Domain: t11-verification
+ *
+ * @package T11EmailVerification
+ */
+
+/**  Copyright 2016
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
